cfe4309877352b385bde7b61f2b0ef173769cbaf
Nguyen Bui <may.thaonguyen2205@gmail.com>
2025-12-03 13:37:40 -0500

    add style

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
-<script setup></script>
+<script setup>
+import Footer from './components/Footer.vue'
+</script>
 
 <template>
+  <div>
+    <NavBar></NavBar>
+    <Footer></Footer>
+  </div>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
@@ -8,4 +14,8 @@
   </p>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container{
+  padding-top: 4.3rem;
+}
+</style>
